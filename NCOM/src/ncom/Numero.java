ef7f77dcafd6a84d1c4be078da9a64c16299c3f1
matiasmarmo <matumarmo@gmail.com>
2019-04-21 21:33:47 -0300

    Primer entrega - TRANSFORMACIONES

---
+++ NCOM/src/ncom/Numero.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ncom;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.atan;
+import static java.lang.Math.cos;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
+/**
+ *
+ * @author Adriana
+ */
+public class Numero {
+    public double a;
+    public double b;
+    public boolean tipo; // FALSE --> BINOMICA
+                         // TRUE ---> POLAR
+
+    Numero(double a, double b, boolean b0) {
+        this.a = a;
+        this.b = b;
+        this.tipo = b0;
+    }
+    
+    public void aPolar(){
+        if(tipo == false){
+            double temp1 = sqrt(pow(a,2)+pow(b,2));
+            double temp2;
+            if(a == 0 && b > 0){
+                temp2 = PI/2;
+            }else if(a == 0 && b < 0){
+                temp2 = 3*PI/2;
+            }else if(b == 0){
+                temp2 = 0;
+            }else if(a>0 && b>0){ //PRIMER CUADRANTE
+                temp2 = atan(b/a);
+            }else if(a>0 && b<0){ //CUARTO CUADRANTE
+                temp2 = atan(b/a) + 2 * PI;
+            }else{ 
+                temp2 = atan(b/a) + PI;
+            }
+            a = floor(temp1*100)/100;
+            b = floor(temp2*100)/100;
+            tipo = true;
+        }
+    }
+    public void aBinomica(){
+        if(tipo == true){
+            double temp1 = a*cos(b);
+            double temp2 = a*sin(b);
+            temp1 = floor(temp1*100)/100;
+            temp2 = floor(temp2*100)/100;
+            a = temp1;
+            b = temp2;
+            tipo = false;
+        }
+    }
+    
+    public void transformar(){
+        if(tipo == false){
+            this.aPolar();
+        }
+        else{
+            this.aBinomica();
+        }
+    }
+}
---
+++ NCOM/src/ncom/parser.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ncom;
+
+/**
+ *
+ * @author Adriana
+ */
+public class parser {
+    public String mensaje;
+    parser(String mensaje){
+        this.mensaje = mensaje;
+    }
+    public int armarNumero(){
+        int i=1;
+        int largoMensaje = this.mensaje.length();
+        if(mensaje.charAt(0) == '(' && mensaje.charAt(largoMensaje-1) == ')'){ 
+            // BINOMICA
+            String datos = mensaje.substring(1,largoMensaje-1);
+            String complejos[] = datos.split(",");
+            String parteReal = complejos[0];
+            String parteImaginaria = complejos[1];
+            if(!this.verificarNumeros(parteReal)){
+                return 0;
+            }
+            if(!this.verificarNumeros(parteImaginaria)){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        else if(mensaje.charAt(0) == '[' && mensaje.charAt(largoMensaje-1) == ']'){
+            //POLAR
+            String datos = mensaje.substring(1,largoMensaje-1);
+            String complejos[] = datos.split(",");
+            String parteReal = complejos[0];
+            String parteImaginaria = complejos[1];
+            if(!this.verificarNumeros(parteReal)){
+                return 0;
+            }
+            if(!this.verificarNumeros(parteImaginaria)){
+                return 0;
+            }
+            if(mensaje.charAt(1)=='-'){
+                return 0;
+            }
+            else{
+                return 2;
+            }
+        }
+        return 0;
+    }
+    private boolean verificarNumeros(String numero){
+        int i;
+        int punto = 0;
+        for(i=0;i<numero.length();i++){
+            if(numero.charAt(i) < '0' && numero.charAt(i) > '9' && numero.charAt(i) != '.' && punto < 2 && numero.charAt(i) != '-'){  
+                return false;
+            }
+            if(numero.charAt(i) == '.'){ punto++; } 
+        }
+        return true;
+    }
+}
